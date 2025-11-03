48f0ffb8abbef6481f79f48c083e4e518befbe8a
Rio Liando <166204479+Rilliaa@users.noreply.github.com>
2025-11-03 20:43:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
-from app.app import *
+import streamlit as st
+import traceback
+
+try:
+    from app.app import *
+except Exception as e:
+    st.error("🚨 Terjadi error saat memuat modul utama:")
+    st.code(traceback.format_exc())
