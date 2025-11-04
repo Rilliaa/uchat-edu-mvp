4acb23d3edc3832ed8619cb37e8cee89a2265a0c
Rio Liando <166204479+Rilliaa@users.noreply.github.com>
2025-11-04 13:24:38 +0700

    Refactor model path construction in load_tfidf function
    Updated the model path to use os.path.abspath for better compatibility.

--- app/app.py
+++ app/app.py
@@ -12,7 +12,8 @@ import re
 @st.cache_resource
 
 def load_tfidf():
-    MODEL_PATH = os.path.join(os.path.dirname(__file__), "../result/tf-idf/intent_classifier_tfidf_lr.pkl")
+    MODEL_PATH = os.path.join(os.path.dirname(__file__), "..", "result", "tf-idf", "intent_classifier_tfidf_lr.pkl")
+    MODEL_PATH = os.path.abspath(MODEL_PATH)
     model = joblib.load(MODEL_PATH)
     return model
 
